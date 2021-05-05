bb37d34ea1c129e1d46f757c3b8c2a43ad2120fa
Tom McDonough <tmcdonough@airslip.com>
2021-05-05 15:51:51 +0100

    Added support to add a generic storage repository

---
+++ src/Airslip.Common.Contracts/IStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Airslip.Common.Contracts
+{
+    public interface IStorage<in T> where T : class
+    {
+        Task SaveFileAsync(T value);
+        Task<(Stream, string?)> DownloadToStreamAsync(string name);
+    }
+}
