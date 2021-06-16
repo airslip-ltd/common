9b304520f62ef7664f3834579001c54384a98f8c
Tom McDonough <tmcdonough@airslip.com>
2021-06-16 10:02:40 +0100

    Added common Json file and protobug serializer

---
+++ src/Airslip.Common.Types/Json.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Airslip.Common.Types
+{
+    public static class Json
+    {
+        public const string MediaType = "application/json";
+
+        public static string Serialize(object content)
+        {
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value) ??
+                   throw new InvalidOperationException("Value deserialized to null");
+        }
+    }
+}
