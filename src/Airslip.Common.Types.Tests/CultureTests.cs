58433041bdafb2f0499b33fd8ffbeaeb3eb25748
Tom McDonough <tmcdonough@airslip.com>
2021-08-03 10:41:47 +0100

    Fixed culture not found exception when LCID passed as a parameter

--- src/Airslip.Common.Types.Tests/CultureTests.cs
+++ src/Airslip.Common.Types.Tests/CultureTests.cs
@@ -9,21 +9,21 @@ namespace Airslip.Common.Types.Tests
     public class CultureTests
     {
         [Fact]
-        public void Can_get_british_currency_symbol_from_iso417_currencyCode()
+        public void Can_get_british_currency_symbol_from_iso4217_currencyCode()
         {
             string currencySymbol = Culture.GetCurrencySymbol("GBP");
             currencySymbol.Should().Be("£");
         }
         
         [Fact]
-        public void Can_get_american_currency_symbol_from_iso417_currencyCode()
+        public void Can_get_american_currency_symbol_from_iso4217_currencyCode()
         {
             string currencySymbol = Culture.GetCurrencySymbol("USD");
             currencySymbol.Should().Be("$");
         }
         
         [Fact]
-        public void Can_get_euro_currency_symbol_from_iso417_currencyCode()
+        public void Can_get_euro_currency_symbol_from_iso4217_currencyCode()
         {
             string currencySymbol = Culture.GetCurrencySymbol("EUR");
             currencySymbol.Should().Be("€");
