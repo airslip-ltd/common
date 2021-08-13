4aff835a5319fbbda231e4f234e914550f385bb6
Tom McDonough <tmcdonough@airslip.com>
2021-08-13 16:35:20 +0100

    Added a parameter for json serialization for camel and snake casing

--- src/Airslip.Common.Types/Json.cs
+++ src/Airslip.Common.Types/Json.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Airslip.Common.Types
 {
@@ -7,15 +8,42 @@ namespace Airslip.Common.Types
     {
         public const string MediaType = "application/json";
 
-        public static string Serialize(object content)
+        public static string Serialize(object value, Casing casing = Casing.CAMEL_CASE, Formatting formatting = Formatting.None)
         {
-            return JsonConvert.SerializeObject(content);
+            JsonSerializerSettings jsonSerializerSettings = GetJsonSerializerSettings(casing, formatting);
+
+            return JsonConvert.SerializeObject(value, jsonSerializerSettings);
         }
 
         public static T Deserialize<T>(string value)
         {
             return JsonConvert.DeserializeObject<T>(value) ??
                    throw new InvalidOperationException("Value deserialized to null");
+        }
+        
+        private static JsonSerializerSettings GetJsonSerializerSettings(Casing casing, Formatting formatting)
+        {
+            return casing switch
+            {
+                Casing.CAMEL_CASE => new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    Formatting = formatting
+                },
+                Casing.SNAKE_CASE => new JsonSerializerSettings
+                {
+                    ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    },
+                    Formatting = formatting
+                },
+                _ => new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    Formatting = formatting
+                }
+            };
         }
     }
 }
