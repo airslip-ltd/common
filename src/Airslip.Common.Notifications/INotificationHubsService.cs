54c10e3efd3579cf0361d7e4c70b3b18570c180b
Tom McDonough <tmcdonough@airslip.com>
2021-09-14 12:49:06 +0100

    Moved notification contracts and types to its own package

---
+++ src/Airslip.Common.Notifications/INotificationHubsService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Airslip.Common.Notifications
+{
+    public interface INotificationHubsService
+    {
+        Task<string> CreateRegistrationId();
+        Task<string> CreateRegistration(
+            DeviceTypes deviceType,
+            string deviceToken,
+            string userId,
+            ICollection<string> tags);
+        Task Send(
+            DeviceTypes deviceType,
+            string message,
+            IEnumerable<string>? tags,
+            Dictionary<string, string>? headers = null,
+            CancellationToken cancellationToken = default);
+        Task<string> Schedule(
+            DeviceTypes deviceType,
+            long scheduledTime,
+            string message,
+            IEnumerable<string> tags,
+            Dictionary<string, string>? headers = null,
+            CancellationToken cancellationToken = default);
+        Task Cancel(string scheduledNotificationId);
+        Task DeleteRegistration(string id);
+        Task<IEnumerable<NotificationRegistration>> GetAllRegistrations(int top);
+        Task<IEnumerable<NotificationRegistration>> GetRegistrationsByChannel(string deviceToken, int top);
+        Task<IEnumerable<NotificationRegistration>> GetRegistrationsByTag(string tag, int top);
+    }
+}
