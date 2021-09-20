9fc10d7ab4abb247bff9a75a26f972e5bdbaf58a
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-09-20 16:13:18 +0100

    Fixed smells

--- src/Airslip.Common.Auth.AspNetCore/Schemes/QrCodeAuthenticationScheme.cs
+++ src/Airslip.Common.Auth.AspNetCore/Schemes/QrCodeAuthenticationScheme.cs
@@ -6,10 +6,10 @@ namespace Airslip.Common.Auth.AspNetCore.Schemes
     public class QrCodeAuthenticationSchemeOptions : AuthenticationSchemeOptions
     {
         public const string QrCodeAuthScheme = AirslipSchemeOptions.QrCodeAuthScheme;
-               
-
+        
         public string Environment
         {
+            get => AirslipSchemeOptions.ThisEnvironment;
             set => AirslipSchemeOptions.ThisEnvironment = value;
         }
     }
