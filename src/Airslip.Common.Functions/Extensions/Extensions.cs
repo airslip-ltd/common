1c7369a8e107ca4df041b388b07be5a354574a63
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-09-22 10:48:47 +0100

    Added common functions

---
+++ src/Airslip.Common.Functions/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Airslip.Common.Functions.Extensions
+{
+    public static class Extensions
+    {
+        static readonly string EnvironmentName = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Production";
+
+        public static IConfigurationBuilder AddDefaultConfig(this IConfigurationBuilder builder, string[] args)
+        {
+            return builder
+                .AddJsonFile("appSettings.json", false)
+                .AddJsonFile($"appSettings.{EnvironmentName}.json", true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+        }
+    }   
+}
