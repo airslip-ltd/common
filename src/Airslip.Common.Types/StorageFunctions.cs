e86db9ccca3aeda71cb8e957304dead4fa1e5d7b
Tom McDonough <tmcdonough@airslip.com>
2021-09-29 13:51:05 +0100

    Added blob storage function

---
+++ src/Airslip.Common.Types/StorageFunctions.cs
@@ -0,0 +1,21 @@
+﻿using Airslip.Common.Types.Extensions;
+using System;
+using System.Text;
+
+namespace Airslip.Common.Types
+{
+    public static class StorageFunctions
+    {
+        public static string BuildBlobName(params string[] values)
+        {
+            if(values.Length.Equals(0))
+                return string.Empty;
+            
+            StringBuilder sb = new();
+            foreach (string value in values)
+                sb.Append($"/{value.ToKebabCasing()}");
+
+            return sb.ToString()[1..];
+        }
+    }
+}
