14e82d769de04aef7172b2e23d5254ede6458634
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-05 14:41:20 +0100

    Added common base uri function

--- src/Airslip.Common.Types/Extensions/UtilityExtensions.cs
+++ src/Airslip.Common.Types/Extensions/UtilityExtensions.cs
@@ -1,5 +1,6 @@
 using Airslip.Common.Types.Configuration;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Airslip.Common.Types.Extensions
@@ -20,6 +21,25 @@ namespace Airslip.Common.Types.Extensions
                 .Settings
                 .First(o => o.Key.Equals(name))
                 .Value;
+        }
+        
+        public static string ToBaseUri(this PublicApiSetting setting)
+        {
+            List<string> parts = new()
+            {
+                setting.BaseUri.RemoveLeadingAndTrailing("/"),
+                setting.UriSuffix.RemoveLeadingAndTrailing("/"),
+                setting.Version.RemoveLeadingAndTrailing("/")
+            };
+            parts.RemoveAll(string.IsNullOrWhiteSpace);
+            return string.Join("/", parts);
+        }
+
+        private static string RemoveLeadingAndTrailing(this string fromValue, string removeValue)
+        {
+            if (fromValue.EndsWith(removeValue)) fromValue = fromValue.Remove(fromValue.Length - 1, 1);
+            if (fromValue.StartsWith(removeValue)) fromValue = fromValue[1..];
+            return fromValue;
         }
     }
 }
