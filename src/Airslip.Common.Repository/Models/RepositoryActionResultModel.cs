53e519b579091bacc3d4cc3afd6caec1163e55df
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-06 13:45:32 +0100

    Made repo action model inherit IResponse

--- src/Airslip.Common.Repository/Models/RepositoryActionResultModel.cs
+++ src/Airslip.Common.Repository/Models/RepositoryActionResultModel.cs
@@ -1,10 +1,12 @@
 using Airslip.Common.Repository.Enums;
 using Airslip.Common.Repository.Interfaces;
+using Airslip.Common.Types.Interfaces;
 
 namespace Airslip.Common.Repository.Models
 {
     public record RepositoryActionResultModel<TModel>
     (ResultType ResultType, TModel? CurrentVersion = null,
         TModel? PreviousVersion = null,
-        ValidationResultModel? ValidationResult = null) where TModel : class, IModel;
+        ValidationResultModel? ValidationResult = null) : IResponse 
+        where TModel : class, IModel;
 }
