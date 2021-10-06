1138c34d9fcb7aa0b3e14ac319103a507dd5e085
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-06 15:34:33 +0100

    Created implementations for standard response type usage

---
+++ src/Airslip.Common.Repository/Models/SuccessfulActionResultModel.cs
@@ -0,0 +1,12 @@
+using Airslip.Common.Repository.Enums;
+using Airslip.Common.Repository.Interfaces;
+using Airslip.Common.Types.Interfaces;
+
+namespace Airslip.Common.Repository.Models
+{
+    public record SuccessfulActionResultModel<TModel>(ResultType ResultType, TModel? CurrentVersion = null,
+            TModel? PreviousVersion = null,
+            ValidationResultModel? ValidationResult = null)
+        : RepositoryActionResultModel<TModel>(ResultType, CurrentVersion, PreviousVersion, ValidationResult), ISuccess
+        where TModel : class, IModel;
+}
