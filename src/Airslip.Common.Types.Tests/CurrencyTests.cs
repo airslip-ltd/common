8345f40d9a74f8b562bd4128a0484b8108c315b7
Tom McDonough <tmcdonough@airslip.com>
2021-10-07 14:51:24 +0100

    Add functions for currency converters for decimal and double

--- src/Airslip.Common.Types.Tests/CurrencyTests.cs
+++ src/Airslip.Common.Types.Tests/CurrencyTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace Airslip.Common.Types.Tests
@@ -7,14 +6,14 @@ namespace Airslip.Common.Types.Tests
     public class CurrencyTests
     {
         [Fact]
-        public void Can_convert_decimal_to_unit_value()
+        public void Can_convert_string_decimal_to_unit_value()
         {
             long? pennies = Currency.ConvertToUnit("12.99");
             pennies.Should().Be(1299);
         }
         
         [Fact]
-        public void Can_convert_unit_to_unit_value()
+        public void Can_convert_string_whole_number_to_unit_value()
         {
             long? pennies = Currency.ConvertToUnit("12");
             pennies.Should().Be(1200);
@@ -25,6 +24,38 @@ namespace Airslip.Common.Types.Tests
         {
             long? pennies = Currency.ConvertToUnit("");
             pennies.Should().Be(null);
+        }
+        
+        [Fact]
+        public void Can_convert_double_to_unit_value()
+        {
+            double value = 12.99;
+            long? pennies = Currency.ConvertToUnit(value);
+            pennies.Should().Be(1299);
+        }
+        
+        [Fact]
+        public void Can_convert_decimal_to_unit_value()
+        {
+            decimal value = (decimal)12.99;
+            long? pennies = Currency.ConvertToUnit(value);
+            pennies.Should().Be(1299);
+        }
+        
+        [Fact]
+        public void Can_convert_double_whole_number_to_unit_value()
+        {
+            double value = 12;
+            long? pennies = Currency.ConvertToUnit(value);
+            pennies.Should().Be(1200);
+        }
+        
+        [Fact]
+        public void Can_convert_decimal_whole_number_to_unit_value()
+        {
+            decimal value = 12;
+            long? pennies = Currency.ConvertToUnit(value);
+            pennies.Should().Be(1200);
         }
     }
 }
