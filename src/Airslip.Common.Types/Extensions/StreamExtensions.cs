4713386e7fab6559961716245a505d69fab07337
Tom McDonough <tmcdonough@airslip.com>
2021-10-13 13:18:10 +0100

    Added stream extension to convert an object

---
+++ src/Airslip.Common.Types/Extensions/StreamExtensions.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Airslip.Common.Types.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async Task<T> DeserializeStream<T>(this Stream requestBody) where T : class
+        {
+            StreamReader sr = new(requestBody);
+            string payload = await sr.ReadToEndAsync();
+            return Json.Deserialize<T>(payload);
+        }
+    }
+}
