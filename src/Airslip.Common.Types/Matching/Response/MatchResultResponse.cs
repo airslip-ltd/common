58011a1671a514375195d96ef199bd0e9579165f
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-14 14:53:38 +0100

    Updated match response

--- src/Airslip.Common.Types/Matching/Response/MatchResultResponse.cs
+++ src/Airslip.Common.Types/Matching/Response/MatchResultResponse.cs
@@ -1,8 +1,16 @@
 using Airslip.Common.Types.Interfaces;
 using Airslip.Common.Types.Matching.Data;
+using Airslip.Common.Types.Transaction;
+using System.Collections.Generic;
 
 namespace Airslip.Common.Types.Matching.Response
 {
-    public record MatchResultResponse(string MatchType, string TransactionTrackingId, string MatchTrackingId, 
-        MatchLikelihood MatchLikelihood) : ISuccess;
+    public record MatchResultResponse(string MatchType,
+        string TransactionTrackingId,
+        string MatchTrackingId,
+        MatchLikelihood MatchLikelihood) : ISuccess
+    {
+        public string? UserId { get; set; } = string.Empty;
+        public List<MatchMetadata> Metadata { get; } = new();
+    }
 }
