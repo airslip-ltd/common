8c0ce916af671db5b6782ebdc843dc4aeaa99188
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-19 08:43:30 +0100

    Added match type static class

---
+++ src/Airslip.Common.Types/Matching/Data/MatchTypes.cs
@@ -0,0 +1,8 @@
+namespace Airslip.Common.Types.Matching.Data
+{
+    public static class MatchTypes
+    {
+        public const string QR_CODE = "QrCode";
+        public const string YAPILY = "Yapily";
+    }
+}
