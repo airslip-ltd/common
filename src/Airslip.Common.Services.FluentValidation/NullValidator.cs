7de6f652806d3bd5beb61c07a3762704cb46758a
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-10-19 14:28:26 +0100

    Added base model validator

---
+++ src/Airslip.Common.Services.FluentValidation/NullValidator.cs
@@ -0,0 +1,14 @@
+﻿using Airslip.Common.Repository.Interfaces;
+using Airslip.Common.Repository.Models;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Threading.Tasks;
+
+namespace Airslip.Common.Services.FluentValidation
+{
+    public class NullValidator<TType> : ModelValidatorBase<TType> 
+        where TType : class, IModel
+    {
+        
+    }
+}
