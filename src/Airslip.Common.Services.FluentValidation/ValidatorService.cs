b6275a838e8ccf08e4d3960324ac0cea946c215d
Tom McDonough <tmcdonough@airslip.com>
2021-10-29 12:20:27 +0100

    Added extension to validate a request object

---
+++ src/Airslip.Common.Services.FluentValidation/ValidatorService.cs
@@ -0,0 +1,55 @@
+﻿using Airslip.Common.Types;
+using Airslip.Common.Types.Extensions;
+using Airslip.Common.Types.Failures;
+using Airslip.Common.Types.Interfaces;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airslip.Common.Services.FluentValidation
+{
+    public static class ValidatorService
+    {
+        // public static async Task ValidateFunction<T>(
+        //     this AbstractValidator<T> validator,
+        //     T request,
+        //     HttpResponseData response) where T : class
+        // {
+        //     ValidationResult? validationResult = await validator.ValidateAsync(request);
+        //
+        //     if (validationResult.IsValid)
+        //         return;
+        //
+        //     ErrorResponse[] errors = validationResult.Errors
+        //         .Select(error =>
+        //             new ErrorResponse(
+        //                 error.ErrorCode,
+        //                 error.ErrorMessage,
+        //                 error.CustomState.ToDictionary<object>()))
+        //         .ToArray();
+        //
+        //     await response.WriteAsJsonAsync(errors, HttpStatusCode.BadRequest);
+        // }
+        
+        public static async Task<IResponse> Validate<T>(
+            this AbstractValidator<T> validator,
+            T request) where T : class
+        {
+            ValidationResult? validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.IsValid)
+                return Success.Instance;
+
+            ErrorResponse[] errors = validationResult.Errors
+                .Select(error =>
+                    new ErrorResponse(
+                        error.ErrorCode,
+                        error.ErrorMessage,
+                        error.CustomState.ToDictionary<object>()))
+                .ToArray();
+
+            return new ErrorResponses(errors);
+        }
+    }
+}
