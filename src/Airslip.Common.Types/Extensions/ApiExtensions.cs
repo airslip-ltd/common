8fa8d75b413e2343b5a58314667564cdd8dffacd
Tom McDonough <tmcdonough@airslip.com>
2021-11-04 12:27:22 +0000

    Added merchant types to common types

---
+++ src/Airslip.Common.Types/Extensions/ApiExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Airslip.Common.Types.Extensions
+{
+    public class ApiExtensions
+    {
+        public static T GetConfigurationSection<T>(IConfiguration configuration)
+        {
+            string className = typeof(T).Name;
+            IConfigurationSection configurationSection = configuration.GetSection(className);
+            return configurationSection.Get<T>();
+        }
+    }
+}
