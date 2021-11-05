d4af5d8ec16063d26fac0d101f248bc52f86f11b
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-05 11:26:08 +0000

    Updated Auth Hosted project

--- src/Airslip.Common.Auth.Hosted/Implementations/HostedContextPrincipalLocator.cs
+++ src/Airslip.Common.Auth.Hosted/Implementations/HostedContextPrincipalLocator.cs
@@ -2,6 +2,7 @@ using Airslip.Common.Auth.Data;
 using Airslip.Common.Auth.Interfaces;
 using Airslip.Common.Types;
 using Airslip.Common.Types.Enums;
+using Airslip.Common.Utilities;
 using System.Collections.Generic;
 using System.Security.Claims;
 
@@ -20,7 +21,9 @@ namespace Airslip.Common.Auth.Hosted.Implementations
         {
             List<Claim> claims = new()
             {
-                new Claim(AirslipClaimTypes.CORRELATION,  string.IsNullOrWhiteSpace(_hostedClaimsPrincipal.CorrelationId) ? CommonFunctions.GetId() : _hostedClaimsPrincipal.CorrelationId),
+                new Claim(AirslipClaimTypes.CORRELATION,  
+                    string.IsNullOrWhiteSpace(_hostedClaimsPrincipal.CorrelationId) ? CommonFunctions.GetId() : 
+                        _hostedClaimsPrincipal.CorrelationId),
                 new Claim(AirslipClaimTypes.AIRSLIP_USER_TYPE, AirslipUserType.InternalApi.ToString()),
                 new Claim(AirslipClaimTypes.IP_ADDRESS, _hostedClaimsPrincipal.IpAddress),
                 new Claim(AirslipClaimTypes.ENTITY_ID, _hostedClaimsPrincipal.EntityId),
