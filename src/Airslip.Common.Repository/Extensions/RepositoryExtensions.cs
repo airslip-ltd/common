dc55d29a5a3b8eebfe0bb942d438fbc396486cf5
Tom McDonough <tmcdonough@airslip.com>
2021-11-07 15:36:43 +0000

    Added common fail repository function

--- src/Airslip.Common.Repository/Extensions/RepositoryExtensions.cs
+++ src/Airslip.Common.Repository/Extensions/RepositoryExtensions.cs
@@ -1,4 +1,9 @@
 using Airslip.Common.Repository.Enums;
+using Airslip.Common.Repository.Interfaces;
+using Airslip.Common.Repository.Models;
+using Airslip.Common.Types.Failures;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Airslip.Common.Repository.Extensions
 {
@@ -6,5 +11,15 @@ namespace Airslip.Common.Repository.Extensions
     {
         public static bool IsActive(this EntityStatus entityStatus) 
             => entityStatus == EntityStatus.Active;
+        
+        public static ErrorResponses ToUnsuccessfulResponse<TModel>(
+            this RepositoryActionResultModel<TModel> result, string errorCode = "400") where TModel : class, IModel
+        {
+            ICollection<ErrorResponse> errors = result.ValidationResult!.Results
+                .Select(v => new ErrorResponse(errorCode, v.Message))
+                .ToList();
+
+            return new ErrorResponses(errors);
+        }
     }
 }
