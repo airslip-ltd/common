15ae4aa272c5be4130a9bc6b0288598e2dd6685a
Tom McDonough <tmcdonough@airslip.com>
2021-11-07 16:28:48 +0000

    Deleted email types

--- src/Airslip.Common.Types/EmailAddressRecipient.cs
+++
@@ -1,4 +0,0 @@
-namespace Airslip.Common.Types
-{
-    public record EmailAddressRecipient(string Email, string Name);
-}
--- src/Airslip.Common.Types/EmailOutcome.cs
+++
@@ -1,8 +0,0 @@
-﻿using Airslip.Common.Types.Interfaces;
-
-namespace Airslip.Common.Types
-{
-    public record EmailOutcome(
-        bool Success,
-        string? ErrorReason = null) : ISuccess;
-}
