2520a2ee1fcba9744a15d8740f991287cf5aae78
Tom McDonough <tmcdonough@airslip.com>
2021-11-07 15:10:20 +0000

    Add get params query string

---
+++ src/Airslip.Common.Utilities/Extensions/WebExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Airslip.Common.Utilities.Extensions
+{
+    public static class WebExtensions
+    {
+        public static T GetQueryParams<T>(this string query) where T : class
+        {
+            NameValueCollection nvc = System.Web.HttpUtility.ParseQueryString(query);
+            Dictionary<string, string?> formDictionary = nvc.AllKeys.ToDictionary(p => p!, p => nvc[p]);
+            string json = Json.Serialize(formDictionary);
+            return Json.Deserialize<T>(json);
+        }
+    }
+}
