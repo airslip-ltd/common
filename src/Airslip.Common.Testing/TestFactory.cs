52394e7f9a5b12d2f82df359eb23518685b665f7
Tom McDonough <tmcdonough@airslip.com>
2021-11-08 12:38:58 +0000

    Added common functions for testing

---
+++ src/Airslip.Common.Testing/TestFactory.cs
@@ -0,0 +1,42 @@
+﻿using Airslip.Common.Utilities.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.IO;
+
+namespace Airslip.Common.Testing
+{
+    public class TestFactory
+    {
+        public static IConfiguration InitialiseConfiguration(string basePath)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+        
+        public static string GetBasePath(string projectName)
+        {
+            string currentDirectory  = Directory.GetCurrentDirectory();
+            string projectSrcFolder = currentDirectory.Substring(0, currentDirectory.IndexOf(projectName, StringComparison.Ordinal));
+            return Path.Combine(projectSrcFolder, projectName);
+        }
+
+        public static Mock<IOptions<T>>  SetUpOptionSettings<T>(string projectName) where T : class
+        {
+            Mock<IOptions<T>> mockSettings = new();
+            string basePath = GetBasePath(projectName);
+            IConfiguration configuration = InitialiseConfiguration(basePath);
+            T settings = UtilityExtensions.GetConfigurationSection<T>(configuration);
+            
+            mockSettings
+                .Setup(s => s.Value)
+                .Returns(settings);
+
+            return mockSettings;
+        }
+    }
+}
