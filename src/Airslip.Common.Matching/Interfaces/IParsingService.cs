30e5771fbd21e07897d232f5d53d24a4dd0e1bc6
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-08 11:46:34 +0000

    Removed duplicate class ref

--- src/Airslip.Common.Matching/Interfaces/IParsingService.cs
+++
@@ -1,9 +0,0 @@
-using Airslip.Common.Types;
-
-namespace Airslip.Common.Matching.Interfaces
-{
-    public interface IParsingService<TType>
-    {
-        ParsingResult<TType> TryParse(string payload);
-    }
-}
