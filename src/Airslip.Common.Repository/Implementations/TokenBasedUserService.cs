e6f7a7a410810324cd4f5efd4deaeefe325f1dee
Tom McDonough <tmcdonough@airslip.com>
2021-11-09 10:26:41 +0000

    Made changes to repository from comments

--- src/Airslip.Common.Repository/Implementations/TokenBasedUserService.cs
+++ src/Airslip.Common.Repository/Implementations/TokenBasedUserService.cs
@@ -1,6 +1,7 @@
 using Airslip.Common.Auth.Interfaces;
 using Airslip.Common.Auth.Models;
 using Airslip.Common.Repository.Interfaces;
+using Airslip.Common.Types.Enums;
 
 namespace Airslip.Common.Repository.Implementations
 {
@@ -8,9 +9,14 @@ namespace Airslip.Common.Repository.Implementations
     {
         public TokenBasedUserService(ITokenDecodeService<UserToken> tokenDecodeService)
         {
-            UserId = tokenDecodeService.GetCurrentToken().UserId;
+            UserToken token = tokenDecodeService.GetCurrentToken();
+            UserId = token.UserId;
+            EntityId = token.EntityId;
+            AirslipUserType = token.AirslipUserType;
         }
 
         public string? UserId { get; }
+        public string? EntityId { get; }
+        public AirslipUserType? AirslipUserType { get; }
     }
 }
