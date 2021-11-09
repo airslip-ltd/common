261727878823eb579de90229f15cd5ca79655ffd
Tom McDonough <tmcdonough@airslip.com>
2021-11-09 11:35:26 +0000

    Add automapper tests

---
+++ src/Airslip.Common.Services.AutoMapper.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Airslip.Common.Services.AutoMapper.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Can_map_()
+        {
+        }
+
+     
+    }
+}
