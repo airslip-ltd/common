46732cf89fe865d25316fd8cc0ffa15afb97a283
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-12 15:44:01 +0000

    Updated IMatchCalculator to allow for different response types

--- src/Airslip.Common.Matching/Interfaces/IMatchCalculator.cs
+++ src/Airslip.Common.Matching/Interfaces/IMatchCalculator.cs
@@ -1,10 +1,11 @@
 using Airslip.Common.Matching.Data;
 using Airslip.Common.Matching.Enum;
+using Airslip.Common.Types.Interfaces;
 
 namespace Airslip.Common.Matching.Interfaces
 {
     public interface IMatchCalculator
     {
-        MatchLikelihood CalculateMatchLikelihood(MatchPerspective matchPerspective, IMatchable source, IMatchable with);
+        IResponse CalculateMatchLikelihood(MatchPerspective matchPerspective, IMatchable source, IMatchable with);
     }
 }
