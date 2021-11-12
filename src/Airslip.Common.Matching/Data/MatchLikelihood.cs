a299751ee20c54cc34a0263cec743e6cca24b792
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-12 15:44:15 +0000

    Updated MatchLikelihood to include ISuccess

--- src/Airslip.Common.Matching/Data/MatchLikelihood.cs
+++ src/Airslip.Common.Matching/Data/MatchLikelihood.cs
@@ -1,8 +1,9 @@
 using Airslip.Common.Matching.Enum;
+using Airslip.Common.Types.Interfaces;
 using System.Collections.Generic;
 
 namespace Airslip.Common.Matching.Data
 {
     public record MatchLikelihood(string MatchSource, MatchPerspective MatchPerspective, string SourceTrackingId,
-        string WithTrackingId, int Score, List<MatchMetric> Metrics);
+        string WithTrackingId, int Score, List<MatchMetric> Metrics) : ISuccess;
 }
