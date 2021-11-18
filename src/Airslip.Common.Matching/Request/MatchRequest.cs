e3973c80f752b9e1ce33d8707aea16c72b772a4a
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-18 14:46:20 +0000

    Added timestamp

--- src/Airslip.Common.Matching/Request/MatchRequest.cs
+++ src/Airslip.Common.Matching/Request/MatchRequest.cs
@@ -7,19 +7,13 @@ namespace Airslip.Common.Matching.Request
     public class MatchRequest
     {
         public string? TrackingId { get; set; }
-
+        public long? Timestamp { get; set; }
         public string MatchType { get; set; } = string.Empty;
-        
         public string EntityId { get; set; } = string.Empty;
-        
         public AirslipUserType AirslipUserType { get; set; } = AirslipUserType.Merchant;
-        
         public string StoreId { get; set; } = string.Empty;
-        
         public string CheckoutId { get; set; } = string.Empty;
-
         public string CallbackUrl { get; set; } = string.Empty;
-        
         public List<MatchMetadata> Metadata { get; set; } = new();
     }
 }
