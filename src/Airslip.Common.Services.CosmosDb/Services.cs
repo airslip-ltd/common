0276cc44f1846396846c854e97058672f5424647
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-11-24 15:00:15 +0000

    Add missing service extension

--- src/Airslip.Common.Services.CosmosDb/Services.cs
+++ src/Airslip.Common.Services.CosmosDb/Services.cs
@@ -11,13 +11,15 @@ namespace Airslip.Common.Services.CosmosDb
     public static class Services
     {
         public static IServiceCollection AddAirslipCosmosDb<TContext>(this IServiceCollection services, 
-            IConfiguration config, Func<Database, Task> initialiseCollections)
+            IConfiguration config, Func<Database, Task> initialiseCollections, 
+            ConnectionMode connectionMode = ConnectionMode.Direct,
+            ConsistencyLevel consistencyLevel = ConsistencyLevel.Session)
         where TContext : AirslipCosmosDbBase
         {
             services
                 .Configure<CosmosDbSettings>(config.GetSection(nameof(CosmosDbSettings)))
                 .AddSingleton(Helpers
-                    .InitializeCosmosClientInstanceAsync(config, initialiseCollections)
+                    .InitializeCosmosClientInstanceAsync(config, initialiseCollections, connectionMode, consistencyLevel)
                     .GetAwaiter()
                     .GetResult())
                 .AddSingleton<TContext>()
