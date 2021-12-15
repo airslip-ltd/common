3949c9f809adb60fa664785209d1393592fd0e4e
Tom McDonough <tmcdonough@airslip.com>
2021-12-15 17:26:31 +0000

    Add query string extension for getting query params

--- src/Airslip.Common.Security.Tests/HmacCipherTests.cs
+++ src/Airslip.Common.Security.Tests/HmacCipherTests.cs
@@ -1,7 +1,9 @@
 using Airslip.Common.Security.Implementations;
+using Airslip.Common.Utilities.Extensions;
 using FluentAssertions;
 using Microsoft.Extensions.Primitives;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Airslip.Common.Security.Tests
@@ -51,6 +53,22 @@ namespace Airslip.Common.Security.Tests
 
             string hmac = "A17AC303A0E3957183F9BF58B20203BFB38AFA890FECE30BD35BECD748A10665";
             bool isValid = HmacCipher.Validate(queryStrings, hmac, SecretKey);
+            isValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Can_remove_hmac_value_and_verify()
+        {
+            string queryString =
+                "code=d5a16abfbe15965ddd272a37cdce8f68&hmac=d3364df5bb6541ad06e7ae51cf522f5567ce67b931f84ccab31711bea0c807d8&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&timestamp=1639585552&state=id";
+
+            List<KeyValuePair<string, string>> queryStrings = queryString.GetQueryParams()?.ToList()!;
+
+            KeyValuePair<string, string> hmacKeyValuePair = queryStrings.Get("hmac");
+
+            queryStrings.Remove(hmacKeyValuePair);
+        
+            bool isValid = HmacCipher.Validate(queryStrings, hmacKeyValuePair.Value, "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");
             isValid.Should().BeTrue();
         }
     }
