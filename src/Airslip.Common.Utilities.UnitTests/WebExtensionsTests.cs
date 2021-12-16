d9cdebb2cbb32b4b2b8073407e54c52bc3468cb0
Tom McDonough <tmcdonough@airslip.com>
2021-12-16 12:13:52 +0000

    Changed get query params to exclude base url

--- src/Airslip.Common.Utilities.UnitTests/WebExtensionsTests.cs
+++ src/Airslip.Common.Utilities.UnitTests/WebExtensionsTests.cs
@@ -12,7 +12,7 @@ namespace Airslip.Common.Utilities.UnitTests
         public void Can_get_query_params_of_unordered_query_string()
         {
             string queryString =
-                "code=d5a16abfbe15965ddd272a37cdce8f68&hmac=d3364df5bb6541ad06e7ae51cf522f5567ce67b931f84ccab31711bea0c807d8&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&timestamp=1639585552&state=id";
+                "https://secure.airslip.com?code=d5a16abfbe15965ddd272a37cdce8f68&hmac=d3364df5bb6541ad06e7ae51cf522f5567ce67b931f84ccab31711bea0c807d8&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&timestamp=1639585552&state=id";
 
             IEnumerable<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams()!;
 
@@ -35,7 +35,7 @@ namespace Airslip.Common.Utilities.UnitTests
             string queryString =
                 "code=d5a16abfbe15965ddd272a37cdce8f68&hmac=d3364df5bb6541ad06e7ae51cf522f5567ce67b931f84ccab31711bea0c807d8&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&timestamp=1639585552&state=id";
 
-            List<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams()?.ToList()!;
+            List<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams().ToList();
 
             KeyValuePair<string, string> hmacKeyValuePair = queryStringKeyValuePairs.Get("hmac");
 
