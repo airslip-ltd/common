6463a2547059c8e1130fbc07339298ac24b0f4b1
Tom McDonough <tmcdonough@airslip.com>
2021-12-16 15:06:43 +0000

    ParseQueryString doesnt support base64 so have to hack a solution

--- src/Airslip.Common.Utilities.UnitTests/WebExtensionsTests.cs
+++ src/Airslip.Common.Utilities.UnitTests/WebExtensionsTests.cs
@@ -1,7 +1,9 @@
 ﻿using Airslip.Common.Utilities.Extensions;
 using FluentAssertions;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Web;
 using Xunit;
 
 namespace Airslip.Common.Utilities.UnitTests
@@ -14,7 +16,7 @@ namespace Airslip.Common.Utilities.UnitTests
             string queryString =
                 "https://secure.airslip.com?code=d5a16abfbe15965ddd272a37cdce8f68&hmac=d3364df5bb6541ad06e7ae51cf522f5567ce67b931f84ccab31711bea0c807d8&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&timestamp=1639585552&state=id";
 
-            IEnumerable<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams()!;
+            IEnumerable<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams();
 
             List<KeyValuePair<string, string>> expectedKeyValuePairs = new()
             {
@@ -28,7 +30,38 @@ namespace Airslip.Common.Utilities.UnitTests
 
             queryStringKeyValuePairs.Should().BeEquivalentTo(expectedKeyValuePairs);
         }
-        
+
+        [Fact]
+        public void Can_validate_string_with_base_64_encoded_string()
+        {
+            string queryString =
+                "https://secure.vendhq.com/connect?response_type=code&client_id=SrSLyYuwnffktH2oGJEJbQTiCXzkHgoL&redirect_uri=override-url&state=YXYdMjx6zDIdw3rxBwAXCocwhShDmcr3fN1NLrJHma6PM+jRwdzIMTqbo8xfiRaTY2aGlqv5u7Re8O82T1tzpqqpJhr3ORirkJK6JL3DBmw=";
+
+            IEnumerable<KeyValuePair<string, string>> queryStringKeyValuePairs = queryString.GetQueryParams(true);
+
+            List<KeyValuePair<string, string>> expectedKeyValuePairs = new()
+            {
+                new KeyValuePair<string, string>("response_type", "code"),
+                new KeyValuePair<string, string>("client_id", "SrSLyYuwnffktH2oGJEJbQTiCXzkHgoL"),
+                new KeyValuePair<string, string>("redirect_uri", "override-url"),
+                new KeyValuePair<string, string>("state", "YXYdMjx6zDIdw3rxBwAXCocwhShDmcr3fN1NLrJHma6PM+jRwdzIMTqbo8xfiRaTY2aGlqv5u7Re8O82T1tzpqqpJhr3ORirkJK6JL3DBmw=")
+            };
+
+            queryStringKeyValuePairs.Should().BeEquivalentTo(expectedKeyValuePairs);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            string str =
+                "https://secure.vendhq.com/connect?response_type=code&client_id=SrSLyYuwnffktH2oGJEJbQTiCXzkHgoL&redirect_uri=override-url&state=YXYdMjx6zDIdw3rxBwAXCocwhShDmcr3fN1NLrJHma6PM+jRwdzIMTqbo8xfiRaTY2aGlqv5u7Re8O82T1tzpqqpJhr3ORirkJK6JL3DBmw=";
+
+           string encodedUrl = HttpUtility.UrlEncode(str);
+            
+          NameValueCollection nvc =  HttpUtility.ParseQueryString( encodedUrl);
+          
+          
+        }        
         [Fact]
         public void Can_remove_key_value_pair()
         {
--- src/Airslip.Common.Utilities/Extensions/StringExtensions.cs
+++ src/Airslip.Common.Utilities/Extensions/StringExtensions.cs
@@ -70,5 +70,14 @@ namespace Airslip.Common.Utilities.Extensions
 
             return splitArray.Select(s => s.Trim());
         }
+        
+        public static string ReplaceFirst(this string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search, StringComparison.Ordinal);
+            if (pos < 0)
+                return text;
+
+            return text[..pos] + replace + text[(pos + search.Length)..];
+        }
     }
 }
