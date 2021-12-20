0f86939cda8c2286830dbfffba92c40b673e99e9
Graham Whitehouse <gwhitehouse@airslip.com>
2021-12-20 16:01:19 +0000

    Split usage of search

--- src/Airslip.Common.Services.CosmosDb/Services.cs
+++ src/Airslip.Common.Services.CosmosDb/Services.cs
@@ -10,22 +10,35 @@ namespace Airslip.Common.Services.CosmosDb
 {
     public static class Services
     {
-        public static IServiceCollection AddAirslipCosmosDb<TContext>(this IServiceCollection services, 
-            IConfiguration config, Func<Database, Task> initialiseCollections, 
+        public static CosmosDbInitialisation AddAirslipCosmosDb<TContext>(this IServiceCollection services,
+            IConfiguration config, Func<Database, Task> initialiseCollections,
             ConnectionMode connectionMode = ConnectionMode.Direct,
             ConsistencyLevel consistencyLevel = ConsistencyLevel.Session)
-        where TContext : AirslipCosmosDbBase
+            where TContext : AirslipCosmosDbBase
         {
             services
                 .Configure<CosmosDbSettings>(config.GetSection(nameof(CosmosDbSettings)))
                 .AddSingleton(Helpers
-                    .InitializeCosmosClientInstanceAsync(config, initialiseCollections, connectionMode, consistencyLevel)
+                    .InitializeCosmosClientInstanceAsync(config, initialiseCollections, connectionMode,
+                        consistencyLevel)
                     .GetAwaiter()
                     .GetResult())
                 .AddSingleton<TContext>()
                 .AddSingleton<IContext>(provider => provider.GetService<TContext>()!);
 
-            return services;
+            return new CosmosDbInitialisation(services, config);
+        }
+
+        public static IServiceCollection UseSearch<TContext>(this CosmosDbInitialisation initialisation)
+            where TContext : AirslipCosmosDbSearchBase
+        {
+            initialisation.services
+                .AddScoped<TContext>()
+                .AddSingleton<ISearchContext>(provider => provider.GetService<TContext>()!);
+
+            return initialisation.services;
         }
     }
+
+    public record CosmosDbInitialisation(IServiceCollection services, IConfiguration config);
 }
