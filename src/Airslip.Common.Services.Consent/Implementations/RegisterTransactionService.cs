e0abc2db82ab520fb00f5fac36630afb60ba32db
Tom McDonough <tmcdonough@airslip.com>
2021-12-20 13:31:08 +0000

    Added logging for production error

--- src/Airslip.Common.Services.Consent/Implementations/RegisterTransactionService.cs
+++ src/Airslip.Common.Services.Consent/Implementations/RegisterTransactionService.cs
@@ -4,6 +4,7 @@ using Airslip.Common.Services.Consent.Enums;
 using Airslip.Common.Services.Consent.Interfaces;
 using Airslip.Common.Utilities;
 using Airslip.Common.Utilities.Extensions;
+using Serilog;
 using System;
 using System.Threading.Tasks;
 
@@ -11,36 +12,44 @@ namespace Airslip.Common.Services.Consent.Implementations
 {
     public class RegisterTransactionService : IRegisterDataService<Transaction, IncomingTransactionModel>
     {
+        private readonly ILogger _logger;
         private readonly IRepository<Transaction, IncomingTransactionModel> _repository;
 
-        public RegisterTransactionService( 
-            IRepository<Transaction, IncomingTransactionModel> repository)
+        public RegisterTransactionService(
+            IRepository<Transaction, IncomingTransactionModel> repository, ILogger logger)
         {
             _repository = repository;
+            _logger = logger;
         }
-        
+
         public async Task RegisterData(IncomingTransactionModel model, DataSources dataSource)
         {
-            model.DataSource = dataSource;
-            model.TimeStamp = DateTime.UtcNow.ToUnixTimeMilliseconds();
-            
-            if (model is IModelWithOwnership ownedModel)
+            try
             {
-                await _repository.Upsert(model.Id ?? string.Empty, model, ownedModel.UserId);
+                model.DataSource = dataSource;
+                model.TimeStamp = DateTime.UtcNow.ToUnixTimeMilliseconds();
+
+                string id = model.Id ?? string.Empty;
+                if (model is IModelWithOwnership ownedModel)
+                    await _repository.Upsert(id, model, ownedModel.UserId);
+                else
+                    await _repository.Upsert(id, model);
             }
-            else
+
+            catch (Exception e)
             {
-                await _repository.Upsert(model.Id ?? string.Empty, model);
+                _logger.Fatal(e, "Unhandled exception for the data source {DataSource} with data packet {Model}",
+                    dataSource, model);
             }
         }
 
-        public async Task RegisterData(string message, DataSources dataSource)
+        public Task RegisterData(string message, DataSources dataSource)
         {
             // Turn to object
             IncomingTransactionModel incomingTransactionModel = Json
                 .Deserialize<IncomingTransactionModel>(message);
 
-            await RegisterData(incomingTransactionModel, dataSource);
+            return RegisterData(incomingTransactionModel, dataSource);
         }
     }
 }
