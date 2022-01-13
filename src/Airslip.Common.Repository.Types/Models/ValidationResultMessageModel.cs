74f03584caac71936d37cbfee6fcddd6677759b7
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-01-13 14:38:11 +0000

    Added additional props to vlidation

--- src/Airslip.Common.Repository.Types/Models/ValidationResultMessageModel.cs
+++ src/Airslip.Common.Repository.Types/Models/ValidationResultMessageModel.cs
@@ -1,4 +1,7 @@
 namespace Airslip.Common.Repository.Types.Models
 {
-    public record ValidationResultMessageModel(string FieldName, string Message);
+    public record ValidationResultMessageModel(string FieldName, string Message)
+    {
+        public string? ErrorCode { get; init; }
+    };
 }
