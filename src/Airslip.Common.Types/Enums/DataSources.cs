15e80c78fb5a1c9be52032ffc0e5ebd2bd48957e
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-01-13 09:05:28 +0000

    Added some common types

---
+++ src/Airslip.Common.Types/Enums/DataSources.cs
@@ -0,0 +1,10 @@
+using JetBrains.Annotations;
+
+namespace Airslip.Common.Types.Enums;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public enum DataSources
+{
+    Yapily,
+    Unknown
+}
