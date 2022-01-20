75f2fdf8e28e8f38f896b7ed3fa6362d2d9c7089
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-01-20 11:28:21 +0000

    Add missing properties

--- src/Airslip.Common.Types/Transaction/TrackingDetails.cs
+++ src/Airslip.Common.Types/Transaction/TrackingDetails.cs
@@ -1,4 +1,6 @@
+using Airslip.Common.Types.Interfaces;
+
 namespace Airslip.Common.Types.Transaction
 {
-    public record TrackingDetails(string TrackingId);
+    public record TrackingDetails(string TrackingId) : ISuccess;
 }
