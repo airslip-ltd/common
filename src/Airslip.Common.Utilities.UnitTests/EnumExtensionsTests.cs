61a93e81e035e96b568d8e75c20c33ded27f38af
Tom McDonough <tmcdonough@airslip.com>
2022-01-26 12:55:48 +0000

    Added standard function to parse enum ignoring string

---
+++ src/Airslip.Common.Utilities.UnitTests/EnumExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Airslip.Common.Types.Enums;
+using Airslip.Common.Utilities.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Airslip.Common.Utilities.UnitTests
+{
+    public class EnumExtensionsTests
+    {
+        [Theory]
+        [InlineData("shopify")]
+        [InlineData("Shopify")]
+        public void Can_remove_accents_from_string(string posProvider)
+        {
+            bool canParse = EnumExtensions.TryParseIgnoreCase(posProvider, out PosProviders provider);
+
+            canParse.Should().BeTrue();
+            Assert.True(provider == PosProviders.Shopify);
+        }
+    }
+}
