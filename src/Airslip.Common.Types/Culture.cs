377743f257122042bff82c7bcf132f7efff6d556
Tom McDonough <tmcdonough@airslip.com>
2022-03-11 10:30:17 +0000

    Removed CompositeId and GetCultureInfoFromCurrencyCode

--- src/Airslip.Common.Types/Culture.cs
+++ src/Airslip.Common.Types/Culture.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
-using Airslip.Common.Types.Enums;
 
 namespace Airslip.Common.Types
 {
@@ -22,27 +21,6 @@ namespace Airslip.Common.Types
                 .Select(t => t.region).First();
             
             return regionInfo.CurrencySymbol;
-        }
-
-        /// <summary>
-        /// Only to be used to get culture info for parsing datetime
-        /// </summary>
-        /// <param name="currencyCode"></param>
-        /// <returns cref="CultureInfo"></returns>
-        /// <exception cref="FormatException"></exception>
-        public static CultureInfo GetCultureInfoFromCurrencyCode(string currencyCode)
-        {
-            if (!Enum.TryParse(currencyCode, out Iso4217CurrencyCodes iso4217CurrencyCodes))
-                throw new FormatException($"Unable to parse {currencyCode} as enum {nameof(Iso4217CurrencyCodes)}");
-
-            return iso4217CurrencyCodes switch
-            {
-                Iso4217CurrencyCodes.GBP => new CultureInfo("en-GB"),
-                Iso4217CurrencyCodes.USD => new CultureInfo("en-US"),
-                // Spanish is used because its the most popular language in EU
-                Iso4217CurrencyCodes.EUR => new CultureInfo("es-ES"),
-                _ => throw new FormatException($"The currency code {currencyCode} is unsupported")
-            };
         }
     }
 }
