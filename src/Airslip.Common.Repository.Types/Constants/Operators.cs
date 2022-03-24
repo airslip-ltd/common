8706d198ef5b4a70e876e9416db9ea9f9e4c9537
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-24 11:24:52 +0000

    Added additional operators

--- src/Airslip.Common.Repository.Types/Constants/Operators.cs
+++ src/Airslip.Common.Repository.Types/Constants/Operators.cs
@@ -9,6 +9,11 @@ public static class Operators
     public const string OPERATOR_LESS_THAN = "<";
     public const string OPERATOR_LESS_THAN_EQUALS = "<=";
     
+    public const string OPERATOR_AFTER = "after";
+    public const string OPERATOR_ON_OR_AFTER = "onOrAfter";
+    public const string OPERATOR_BEFORE = "before";
+    public const string OPERATOR_ON_OR_BEFORE = "onOrBefore";
+    
     public const string LINK_OPERATOR_AND = "and";
     public const string LINK_OPERATOR_OR = "or";
 }
