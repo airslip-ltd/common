7785d4d770d673b137e6994af6aa583cea9b09ac
Tom McDonough <tmcdonough@airslip.com>
2022-04-01 11:30:48 +0100

    Add common error type

---
+++ src/Airslip.Common.Types/Failures/HandledError.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Airslip.Common.Types.Failures;
+
+public class HandledError : ErrorResponse
+{
+    public HandledError(string codeLocation, string message)
+        : base(
+            "HANDLED_ERROR",
+            message,
+            new Dictionary<string, object>
+            {
+                { "resource", codeLocation },
+                { "validation", message }
+            }
+        )
+    {
+    }
+}
