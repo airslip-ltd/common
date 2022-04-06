099ccdc0f2e61ff3636becf2626a21e2c35ee163
Graham Whitehouse <gwhitehouse@airslip.com>
2022-04-06 09:46:43 +0100

    Add utility extensions

--- src/Airslip.Common.Utilities/Extensions/StringExtensions.cs
+++ src/Airslip.Common.Utilities/Extensions/StringExtensions.cs
@@ -89,5 +89,15 @@ namespace Airslip.Common.Utilities.Extensions
         {
             return Enum.Parse<TEnum>(value, true);
         }
+        
+        public static string Base64Encode(this string plainText) {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+        
+        public static string Base64Decode(this string base64EncodedData) {
+            byte[] base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
     }
 }
