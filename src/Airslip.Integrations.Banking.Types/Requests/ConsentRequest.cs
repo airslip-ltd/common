06b1a316188d16e0ca63468368900aa672b4b6cd
Graham Whitehouse <gwhitehouse@airslip.com>
2022-04-06 15:49:24 +0100

    Normalise wording

--- src/Airslip.Integrations.Banking.Types/Requests/ConsentRequest.cs
+++ src/Airslip.Integrations.Banking.Types/Requests/ConsentRequest.cs
@@ -7,7 +7,7 @@ namespace Airslip.Integrations.Banking.Types.Requests
         public AirslipUserType AirslipUserType { get; set; } = AirslipUserType.Standard;
         public string? EntityId { get; set; }
         public string UserId { get; set; } = string.Empty;
-        public string InstitutionId { get; set; } = string.Empty;
+        public string BankingBankId { get; set; } = string.Empty;
         public string? CallbackUrl { get; set; }
     }
 }
