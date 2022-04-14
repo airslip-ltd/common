cc5b71de3158c7e49753aeb9474e70c9d7104e1a
Tom McDonough <tmcdonough@airslip.com>
2022-04-14 12:02:22 +0100

    Added a conversion enum type to stringcipher so we can encrypt with an output of only alphanumerical characters

--- src/Airslip.Common.Security.Tests/StringCipherTests.cs
+++ src/Airslip.Common.Security.Tests/StringCipherTests.cs
@@ -1,34 +1,62 @@
+using Airslip.Common.Security.Enums;
 using Airslip.Common.Security.Implementations;
 using FluentAssertions;
+using System.Text.RegularExpressions;
 using System.Web;
 using Xunit;
 
-namespace Airslip.Common.Security.Tests
+namespace Airslip.Common.Security.Tests;
+
+public class StringCipherTests
 {
-    public class StringCipherTests
+    private const string PassPhrase = "SECRET_KEY";
+        
+    [Fact]
+    public void Can_encrypt_and_decrypt_string_value()
     {
-        private const string PassPhrase = "SECRET_KEY";
+        string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
+        string encryptedString = StringCipher.Encrypt(str, PassPhrase);
+        string decryptedString = StringCipher.Decrypt(encryptedString, PassPhrase);
+        decryptedString.Should().Be(str);
+    }
         
-        [Fact]
-        public void Can_encrypted_and_decrypt_string_value()
-        {
-            string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
-            string encryptedString = StringCipher.Encrypt(str, PassPhrase);
-            string decryptedString = StringCipher.Decrypt(encryptedString, PassPhrase);
-            decryptedString.Should().Be(str);
-        }
+    [Fact]
+    public void Can_encrypt_and_decrypt_string_value_for_url()
+    {
+        string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
+        string encryptedString = StringCipher.EncryptForUrl(str, PassPhrase);
+            
+        // Happens automatically during API request
+        string urlDecodedEncryptedString = HttpUtility.UrlDecode(encryptedString);
+            
+        string decryptedString = StringCipher.Decrypt(urlDecodedEncryptedString, PassPhrase);
+        decryptedString.Should().Be(str);
+    }
         
-        [Fact]
-        public void Can_encrypted_and_decrypt_string_value_for_url()
-        {
-            string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
-            string encryptedString = StringCipher.EncryptForUrl(str, PassPhrase);
+    [Fact]
+    public void Can_encrypt_and_decrypt_string_value_using_hex_conversion()
+    {
+        string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
+        string encryptedString = StringCipher.Encrypt(str, PassPhrase, ConversionType.Hex);
+        Regex alphanumericRegex = new("^[a-zA-Z0-9]*$");
+        bool onlyContainsAlphanumericValues = alphanumericRegex.IsMatch(encryptedString);
+        onlyContainsAlphanumericValues.Should().BeTrue();
+        string decryptedString = StringCipher.Decrypt(encryptedString, PassPhrase, ConversionType.Hex);
+        decryptedString.Should().Be(str);
+    }
+        
+    [Fact]
+    public void Can_encrypt_and_decrypt_string_value_for_url_using_hex_conversion()
+    {
+        string str = "{\"userType\":2,\"entityId\":\"entity-id\"}";
+        string encryptedString = StringCipher.EncryptForUrl(str, PassPhrase, ConversionType.Hex);
+        Regex alphanumericRegex = new("^[a-zA-Z0-9]*$");
+        bool onlyContainsAlphanumericValues = alphanumericRegex.IsMatch(encryptedString);
+        onlyContainsAlphanumericValues.Should().BeTrue();
+        // Happens automatically during API request
+        string urlDecodedEncryptedString = HttpUtility.UrlDecode(encryptedString);
             
-            // Happens automatically during API request
-            string urlDecodedEncryptedString = HttpUtility.UrlDecode(encryptedString);
-            
-            string decryptedString = StringCipher.Decrypt(urlDecodedEncryptedString, PassPhrase);
-            decryptedString.Should().Be(str);
-        }
+        string decryptedString = StringCipher.Decrypt(urlDecodedEncryptedString, PassPhrase, ConversionType.Hex);
+        decryptedString.Should().Be(str);
     }
 }
