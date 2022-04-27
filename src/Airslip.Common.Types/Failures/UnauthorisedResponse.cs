a7f1c2ac292975b18450d977cec439d3914f8d0a
Tom McDonough <tmcdonough@airslip.com>
2022-04-27 17:03:43 +0100

    Added empty constructor so our error responses can be deserialised

--- src/Airslip.Common.Types/Failures/UnauthorisedResponse.cs
+++ src/Airslip.Common.Types/Failures/UnauthorisedResponse.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections.Generic;
 
-namespace Airslip.Common.Types.Failures
+namespace Airslip.Common.Types.Failures;
+
+public class UnauthorisedResponse : ErrorResponse
 {
-    public class UnauthorisedResponse : ErrorResponse
+    public UnauthorisedResponse()
     {
-        public UnauthorisedResponse(string resource, string validation)
-            : base(
-                "UNAUTHORISED",
-                "Access is not allowed to the {resource} resource. {validation}.",
-                new Dictionary<string, object>
-                {
-                    { "resource", resource }, 
-                    { "validation", validation }
-                })
-        {
-        }
+            
+    }
+        
+    public UnauthorisedResponse(string resource, string validation)
+        : base(
+            "UNAUTHORISED",
+            "Access is not allowed to the {resource} resource. {validation}.",
+            new Dictionary<string, object>
+            {
+                { "resource", resource }, 
+                { "validation", validation }
+            })
+    {
     }
 }
